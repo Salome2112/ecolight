a9254761ae37c87e65e476074234fe4781692dfd
Salome2112 <146013347+Salome2112@users.noreply.github.com>
2024-07-05 07:31:22 -0500

    s

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+ 
+
+const consumos = [
+    {
+      nameClient: "Noemi Lucero",
+      clientId: 1,
+      codUniqueElectric: 123456,
+      meterNumber: 1234.56,
+      typeConsumption: "Residential",
+      startDate: new Date("2024-06-23"),
+      endDate: new Date("2024-07-23"),
+      description: "Monthly electricity consumption",
+      previousReading: 1000.0,
+      currentReading: 1200.0,
+      contractAccount: 987654321012345,
+      billedDays: 31,
+      consumptionTotal: 200,
+      total:30.60
+    },
+]
